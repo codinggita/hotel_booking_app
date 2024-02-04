7c68317c2ce907cb439a6a60a08a5baec1808d5b
Aditya Raj <aditya13raja@gmail.com>
2024-02-05 00:29:01 +0530

    Created search-box with calender

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,11 +1,17 @@
 import Footer from "../footer/Footer";
+import Header from "../header/Header";
+import './home.scss';
+
 
 function Home() {
   return (
-    <div>
+    <div className="home">
+      <div className="header-text">
+        <h2>We compare hotel prices from 100s of sites</h2>
+        <p>We’ll do the searching. You do the saving.</p>
+      </div>
       
-      <h2>We compare hotel prices from 100s of sites</h2>
-      <p>We’ll do the searching. You do the saving.</p>
+      <Header/>
       
 
       <Footer/>
